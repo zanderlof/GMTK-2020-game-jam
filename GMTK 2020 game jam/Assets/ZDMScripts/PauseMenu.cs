dca28862f72c264fb785512507a6bd87cb35b4fe
ZDMGames <39507120+ZDMGames@users.noreply.github.com>
2020-07-10 17:24:57 -0400

    Pause & Main Menu Setup

---
+++ GMTK 2020 game jam/Assets/ZDMScripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool isPaused = false;
+    public GameObject pauseMenu;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Cancel") && !isPaused)
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+            pauseMenu.SetActive(true);
+        }
+        else if(Input.GetButtonDown("Cancel") && isPaused)
+        {
+            ResumeGame();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        pauseMenu.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Time.timeScale = 1;
+        Application.Quit();
+    }
+}
