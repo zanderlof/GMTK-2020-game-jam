153359a92cc92e97505bd22c37112e36ecac3fd8
zanderlof <zanderlof@gmail.com>
2020-07-11 23:43:16 -0700

    doors and rooms
    added doors with the ability to open and close them. also conected all doors to rooms

--- GMTK 2020 game jam/Assets/ZDMScripts/Door.cs
+++ GMTK 2020 game jam/Assets/ZDMScripts/Door.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.U2D;
 
@@ -8,19 +9,33 @@ public class Door : MonoBehaviour
     public bool isOpened;
     public List<RoomManager> connectingRooms;
 
+    public GameObject player;
+    public float interactRadius;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (math.abs(player.transform.position.x - transform.position.x) < interactRadius && math.abs(player.transform.position.y - transform.position.y) < interactRadius)
+            {
+                OpenDoor();
+            }
+        }
+    }
+
     public void OpenDoor()
     {
         if (isOpened)
         {
             isOpened = false;
             this.GetComponent<BoxCollider2D>().enabled = true;
-            this.GetComponent<SpriteShapeRenderer>().enabled = true;
+            this.GetComponent<SpriteRenderer>().enabled = true;
         }
         else
         {
             isOpened = true;
             this.GetComponent<BoxCollider2D>().enabled = false;
-            this.GetComponent<SpriteShapeRenderer>().enabled = false;
+            this.GetComponent<SpriteRenderer>().enabled = false;
         }
     }
 }
