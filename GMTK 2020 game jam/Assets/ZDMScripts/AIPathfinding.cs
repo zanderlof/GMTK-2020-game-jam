c4fb54afd1b6693cb831cbe2d7e7cea852e2641e
ZDMGames <39507120+ZDMGames@users.noreply.github.com>
2020-07-12 04:13:07 -0400

    Movement and AI Doors

--- GMTK 2020 game jam/Assets/ZDMScripts/AIPathfinding.cs
+++ GMTK 2020 game jam/Assets/ZDMScripts/AIPathfinding.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AIPathfinding : MonoBehaviour
 {
+    public float moveSpeed;
     public PathNode targetLocation;
     public PathNode currentLocation;
     public List<PathNode> steps;
@@ -12,6 +13,27 @@ public class AIPathfinding : MonoBehaviour
     void Start()
     {
         FindPath();
+    }
+
+    private void FixedUpdate()
+    {
+        if(currentLocation != targetLocation)
+        {
+            this.transform.position = Vector2.MoveTowards(this.transform.position, steps[steps.Count - 1].transform.position, moveSpeed * Time.deltaTime);
+            if(this.transform.position == steps[steps.Count - 1].transform.position)
+            {
+                currentLocation = steps[steps.Count - 1];
+                steps.Remove(steps[steps.Count - 1]);
+            }
+        } 
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Door")
+        {
+            collision.gameObject.GetComponent<Door>().OpenDoor();
+        }
     }
 
     public void FindPath()
