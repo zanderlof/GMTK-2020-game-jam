b1efbb61fa641872c452313f0ead693d6116e2ba
zanderlof <zanderlof@gmail.com>
2020-07-11 20:28:47 -0700

    welder and flame thrower
    Added a welder and flame thrower to the game

---
+++ GMTK 2020 game jam/Assets/Scripts/Fire.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public ParticleSystem fire;
+    public ParticleSystem extinguisher;
+    public List<ParticleCollisionEvent> collisionEvents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.lossyScale.x < Vector3.one.x * 0.1f && transform.lossyScale.y < Vector3.one.y * 0.1f)
+        {
+            Destroy(gameObject);
+        }
+        
+        /*
+         * if particle collision
+         *      grow size
+         */
+        
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        int numFireCollisionEvents = fire.GetCollisionEvents(other, collisionEvents);
+        int numExtinguisherCollisionEvents = extinguisher.GetCollisionEvents(other, collisionEvents);
+
+
+        int i = 0;
+
+        while (i < numFireCollisionEvents)
+        {
+           
+                transform.localScale *= 1.1f;
+            
+            i++;
+        }
+
+        i = 0;
+        
+        while (i < numExtinguisherCollisionEvents)
+        {
+
+            transform.localScale *= 0.9f;
+
+            i++;
+        }
+    }
+}
--- GMTK 2020 game jam/Assets/Scripts/Weapon.cs
+++ GMTK 2020 game jam/Assets/Scripts/Weapon.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.WSA;
 
-public class Tool : MonoBehaviour
+public class Weapon : MonoBehaviour
 {
     public GameObject player;
     public Player player2;
+
+    public GameObject particleSystem;
 
     public float pickupRadius;
     private bool held;
@@ -17,15 +19,16 @@ public class Tool : MonoBehaviour
     {
         held = false;
     }
-    
+
     // Update is called once per frame
     void Update()
     {
+        //pick up or drop the item
         if (Input.GetKeyDown(KeyCode.E))
         {
             if (!held)
             {
-                if (math.abs(player.transform.position.x - transform.position.x) < pickupRadius && math.abs(player.transform.position.y - transform.position.y) < pickupRadius )
+                if (math.abs(player.transform.position.x - transform.position.x) < pickupRadius && math.abs(player.transform.position.y - transform.position.y) < pickupRadius)
                 {
                     held = true;
                     player2.Holding(gameObject);
@@ -36,6 +39,16 @@ public class Tool : MonoBehaviour
                 player2.DropHeld();
                 held = false;
             }
+        }
+
+        //Activate the wepon
+        if (held && Input.GetKeyDown(KeyCode.Q))
+        {
+            particleSystem.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            particleSystem.SetActive(false);
         }
     }
 }
